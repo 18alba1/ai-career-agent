6ee7c8d8f2afc1411e2c024adab0e8f6aea42f78
18alba <alexanderbarhado@gmail.com>
2026-09-20 12:40:20 +0200

    added names instead of id

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig({
       "/interview": "http://127.0.0.1:8000",
       "/transcribe-audio": "http://127.0.0.1:8000",
       "/text-to-speech": "http://127.0.0.1:8000",
+      "/candidates": "http://127.0.0.1:8000",
       "/jobs": "http://127.0.0.1:8000",
       "/candidate": "http://127.0.0.1:8000",
       "/database-test": "http://127.0.0.1:8000",
